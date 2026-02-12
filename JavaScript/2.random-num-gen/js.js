ef2f69a6e74f8e5ca43a89b7866e30396c7f05b5
Gauri <gauridale19@gmail.com>
2026-02-12 18:16:35 +0530

    random-num

---
+++ JavaScript/2.random-num-gen/js.js
@@ -0,0 +1,13 @@
+// RANDOM NUMBER GENERATOR
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+
+let randomNum;
+
+myButton.onClick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
+}
---
+++ JavaScript/2.random-num-gen/notes.md
@@ -0,0 +1,8 @@
+# This generates a random number between 0 and 1
+`Math.random()` (includes decimals)
+
+# If we want a random number between a particular range
+- For example, between 1 and 6
+`(Math.random() * 6) +1` (this includes decimals)
+
+- Without decimals `Math.floor(Math.random() * 6);`
